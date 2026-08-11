a68e1cb46d7164a238fd1fcc41988459550beee5
phshbone <phshbone@gmail.com>
2026-08-11 14:34:44 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='bills-sketch-pad-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
